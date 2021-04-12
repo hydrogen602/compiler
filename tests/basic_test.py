5dc1a61236319e2d2d99566d1f26e2ad13450ddd
Jonathan Rotter <hydrogen31415@gmail.com>
2021-04-12 20:35:43 +0200

    tests now individual

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -8,31 +8,36 @@ def compile(request):
     result = subprocess.run('make')
     result.check_returncode()
 
-def test_all():
-    thisPath = sys.path[0]
-    casesPath = os.path.join((thisPath), 'cases')
+
+thisPath = sys.path[0]
+casesPath = os.path.join((thisPath), 'cases')
+
+cases = set(t[:t.index('.')] for t in os.listdir(casesPath) if not t.startswith('.'))
 
 
-    test_cases = set(t[:t.index('.')] for t in os.listdir(casesPath) if not t.startswith('.'))
+def case_helper(test):
+    pathToCase = os.path.join(casesPath, test)
 
-    for test in test_cases:
-        pathToCase = os.path.join(casesPath, test)
+    compileResult = subprocess.run(['./Main', '-i', pathToCase + '.idk', '-o', 'tests/tmp.s'], capture_output=True)
 
-        compileResult = subprocess.run(['./Main', '-i', pathToCase + '.idk', '-o', 'tests/tmp.s'], capture_output=True)
+    if test.startswith('fail'):
+        assert compileResult.returncode != 0 # invalid code should fail
+    else:
+        #print(compileResult.stderr.decode())
+        if compileResult != 0: print(test, cases)
+        compileResult.check_returncode()
 
-        if test.startswith('fail'):
-            assert compileResult.returncode != 0 # invalid code should fail
-        else:
-            #print(compileResult.stderr.decode())
-            if compileResult != 0: print(test, test_cases)
-            compileResult.check_returncode()
+        result = subprocess.run(['spim', '-file', 'tests/tmp.s'], capture_output=True)
+        result.check_returncode()
 
-            result = subprocess.run(['spim', '-file', 'tests/tmp.s'], capture_output=True)
-            result.check_returncode()
+        output = result.stdout.decode().split('Loaded: /usr/lib/spim/exceptions.s\n', maxsplit=1)[1]
 
-            output = result.stdout.decode().split('Loaded: /usr/lib/spim/exceptions.s\n', maxsplit=1)[1]
+        with open(pathToCase + '.result.txt') as f:
+            solution = f.read()
+        
+        assert output == solution
 
-            with open(pathToCase + '.result.txt') as f:
-                solution = f.read()
-            
-            assert output == solution
+
+# creates all the tests automatically
+for test in cases:
+    exec(f'def test_{test}():\n    case_helper("{test}")')
